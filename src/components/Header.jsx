70917fd856d22c931950817e46511b601cc25dea
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-13 19:49:36 -0300

    ✨add Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
-export function Header(){
-    return (
-        <strong className={styles.header}>Ignite Feed</strong>
-    )
+import igniteLogo from "../images/ignite-logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite"></img>
+    </header>
+  );
 }
