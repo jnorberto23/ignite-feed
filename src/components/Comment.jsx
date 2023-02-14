b1a9cc089063283df85860552bc395301bd81732
jnorberto23 <joaovictor.norberto@outlook.com>
2023-02-14 18:17:21 -0300

    🚧work in comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Header.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/diego3g.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time
+                title="14 de Fevereiro de 2023 às 16h40"
+                dateTime="2023-02-14 16:40:00"
+              >
+                Cerca de 1h
+              </time>
+            </div>
+            <button title="Deletar comentario">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom, Joao!</p>
+        </div>
+        <footer>
+          <ThumbsUp /> Aplaudir <span>20</span>
+        </footer>
+      </div>
+    </div>
+  );
+}
